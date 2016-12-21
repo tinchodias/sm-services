abd78e017c429c22ad45d9d76dd80ff9b311fa1d
tinchodias <tinchodias@gmail.com>
2016-12-21 02:24:54 -0300

    cleanup

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require("path");
 var bodyParser = require("body-parser");
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
+//app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
 
