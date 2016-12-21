12c98c992fff685537b3ea53b5f551466e237fc0
tinchodias <tinchodias@gmail.com>
2016-12-21 04:16:08 -0300

    try firebase admin

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
+var admin = require("firebase-admin");
 
 var app = express();
 //app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
+
+
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.PROJECT_ID,
+    clientEmail: process.env.CLIENT_EMAIL,
+    privateKey: process.env.PRIVATE_KEY
+  }),
+  databaseURL: process.env.DATABASE_URL
+});
 
 
 // Initialize the app.
@@ -14,7 +26,21 @@ var server = app.listen(process.env.PORT || 8080, function () {
 });
 
 
+
+
+
 app.get("/ping", function(req, res) {
-  console.log("ping received!");
-  res.status(200).json({"result": "buenisimo"});
+
+  admin.auth().getUser("as8Rf5YctVZxwasHBZavj2Ohyw83")
+    .then(function(userRecord) {
+      // See the tables below for the contents of userRecord
+      console.log("Successfully fetched user data:", userRecord.toJSON());
+      res.status(200).json({"result": "bien"});
+    })
+    .catch(function(error) {
+      console.log("Error fetching user data:", error);
+      res.status(200).json({"result": "mal"});
+    });
+
+
 });
