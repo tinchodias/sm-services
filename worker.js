9ae6e472123aa66f3cb6e358dbdb0c6295a7a86b
Martin Dias <tinchodias@gmail.com>
2017-02-24 21:19:23 -0300

    debug

--- worker.js
+++ worker.js
@@ -1,3 +1,33 @@
 
 
-console.log("Worker ready");
+console.log("Worker starting...");
+
+var Queue = require('firebase-queue');
+
+
+/* FIREBASE */
+var admin = require("firebase-admin");
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.PROJECT_ID,
+    clientEmail: process.env.CLIENT_EMAIL,
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
+  }),
+  databaseURL: process.env.DATABASE_URL
+});
+/* FIREBASE */
+
+
+var ref = admin.database().ref('queue');
+var queue = new Queue(ref, function(data, progress, resolve, reject) {
+  // Read and process task data
+  console.log(data);
+
+  // Do some work
+  progress(50);
+
+  // Finish the task asynchronously
+  setTimeout(function() {
+    resolve();
+  }, 1000);
+});
