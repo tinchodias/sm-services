4cd729447f971a1d8550af90d9fd699b3aacf1a9
Martin Dias <tinchodias@gmail.com>
2017-02-26 00:21:30 -0300

    minor code improvement in worker

--- worker.js
+++ worker.js
@@ -26,30 +26,33 @@ var queueRef = admin.database().ref('queue');
 /* FIREBASE */
 
 
-var Queue = require('firebase-queue');
-var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
+/* FIREBASE QUEUE */
+var FirebaseQueue = require('firebase-queue');
+/* FIREBASE QUEUE */
 
-  console.log("processing " + data);
 
+var queue = new FirebaseQueue(queueRef, function(data, progress, resolve, reject) {
+
+  console.log("processing ", data);
 
   admin.database().ref("private/users/" + data.id + "/last_access_token").once("value", function(accessToken) {
-
     progress(25);
-
     smfb.profiles(accessToken.val()).then(function(fbres) {
-
       progress(50);
-
       admin.database().ref("private/users/" + data.id + "/last_data").set({
         'timestamp': (+ new Date()),
         'data': fbres
-      }).then(resolve, reject);
-
-    }, function (error) {
+      }).then(function() {
+        console.log("done ", data);
+        resolve();
+      }, function(error) {
+        console.log("fail ", data);
+        reject(error);
+      });
+    }, function(error) {
       reject(error)
     });
   });
-
 
 });
 
